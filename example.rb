ac2108d5de37547a18839cb631b540f8d912fbab
kakoga <kkoga@alumni.nd.edu>
2016-10-20 15:13:52 -0700

    modern roman numerals and recursion

--- example.rb
+++ example.rb
@@ -110,3 +110,51 @@
 # puts (make_roman_numerals (1999))
 
 # Modern Roman Numerals
+
+# def make_modern_roman_numerals number
+#   thous = (number / 1000)
+#   hunds = (number % 1000 / 100)
+#   tens = (number % 100 / 10)
+#   ones = (number % 10 )
+#   roman = "M" * thous
+#   if hunds == 9
+#     roman = roman + "CM"
+#   elsif hunds == 4
+#     roman = roman " CD"
+#   else
+#     roman = roman + "D" * (number % 1000 / 500)
+#     roman = roman + "C" * (number % 500 / 100)
+#   end
+#   if tens == 9
+#     roman = roman + "XC"
+#   elsif tens == 4
+#     roman = roman + "XL"
+#   else
+#     roman = roman + "L" * (number % 100 / 50)
+#     roman = roman + "X" * (number % 50 / 10)
+#   end
+#   if ones == 9
+#     roman = roman + "IX"
+#   elsif ones == 4
+#     roman = roman "IV"
+#   else
+#     roman = roman + "V" * (number % 10 / 5)
+#     roman = roman + "I" * (number % 5 / 1)
+#   end
+#   roman
+# end
+# puts (make_modern_roman_numerals (1999))
+
+# Recusion - factorial
+
+def calculate_factorial number
+  if number < 0
+    "Please enter a positive number"
+  end
+  if number == 0
+    1
+  else
+    number * calculate_factorial(number-1)
+  end
+end
+puts calculate_factorial(3)
